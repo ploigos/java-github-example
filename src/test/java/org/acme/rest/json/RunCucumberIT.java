31088bc6aa0859c0533097afffcf0f82ec30bea6
Dillon Winchell <dwinchel@redhat.com>
2022-05-09 13:46:39 -0400

    Add example Java application code

---
+++ src/test/java/org/acme/rest/json/RunCucumberIT.java
@@ -0,0 +1,9 @@
+package org.acme.rest.json;
+
+import io.cucumber.junit.Cucumber;
+import org.junit.runner.RunWith;
+
+@io.cucumber.junit.platform.engine.Cucumber
+@RunWith(Cucumber.class)
+public class RunCucumberIT {
+}
